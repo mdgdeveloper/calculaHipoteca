04085a170c9f3ca5ad42ed5b614493f14d15e031
mdgdeveloper <se.rgiomadrigal@gmail.com>
2024-04-06 13:58:30 +0200

    Last update

--- calculahipoteca/app/resultado/page.tsx
+++ calculahipoteca/app/resultado/page.tsx
@@ -5,13 +5,53 @@ import Image from "next/image"
 // Get the values passed as GET parameters
 import { useSearchParams } from 'next/navigation'
 
+// Resultado tipo test: 
+
+const resultado = {
+    "id": 1,
+    "bank": 3,
+    "interestRateType": 1,
+    "interestRate": 1.5,
+    "initialPayment": 50000,
+    "totalPayment": 200000,
+    "totalYears": 30,
+}
+
 
 const page = () => {
     // Get the values passed as GET parameters
     const searchParams = useSearchParams()
+    // Parameters to be obtained 
+    // 1. id
+    // 2. bank
+    // 3. interestRateType
+    // 4. interestRate
+    // 5. totalPayment
+    // 6. initialPayment
+    // 7. totalMonths
+
     const id = searchParams.get("id")
     const bank = searchParams.get("bank")
+    const interestRateType = searchParams.get("interestRateType")
+    const interestRate = searchParams.get("interestRate")
+    const totalPayment = searchParams.get("totalPayment")
+    const initialPayment = searchParams.get("initialPayment")
+    const totalYears = searchParams.get("totalYears")
+
     const bankArray: Number[] = bank ? [Number(bank)] : []
+
+
+    // Calculate the monthly payment based on the data provided
+    const monthlyPayment = () => {
+        // 1. Calculate the interest rate per month
+        const interestRatePerMonth = Number(interestRate) / 12 / 100
+        // 2. Calculate the number of months
+        const numberOfMonths = Number(totalYears) * 12
+        // 3. Calculate the monthly payment
+        const monthlyPayment = (Number(totalPayment) - Number(initialPayment)) * interestRatePerMonth * Math.pow(1 + interestRatePerMonth, numberOfMonths) / (Math.pow(1 + interestRatePerMonth, numberOfMonths) - 1)
+
+        return monthlyPayment
+    }
 
     return (
         <div className="flex justify-center items-center h-screen">
